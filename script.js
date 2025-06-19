0d19c5e252afe178f1fd6d716b83ce4c8930e918
PSGPLAY <shresthaprajwal09@gmail.com>
2025-06-19 13:07:12 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".gift").addEventListener("click", () => {
+   document.getElementById("first").style.display = "none";
+   document.getElementById("second").style.display = "block";
+});
