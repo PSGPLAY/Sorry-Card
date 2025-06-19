590044eb5f0f1d60f96f2503b1bbc0e1ceb60cc5
PSGPLAY <shresthaprajwal09@gmail.com>
2025-06-19 13:49:07 +0545

    Your commit message

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.querySelector(".gift").addEventListener("click", () => {
    document.getElementById("first").style.display = "none";
    document.getElementById("second").style.display = "block";
+   document.getElementById("click-me").style.display = "none";
 });
+
+function back(){
+   setTimeout(() => {
+      document.getElementById("first").style.display = "block";
+      document.getElementById("second").style.display = "none";
+      document.getElementById("click-me").style.display = "block";
+   }, 200);
+}
